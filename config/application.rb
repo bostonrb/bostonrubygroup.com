3619b79127f225621cb7002d5cc38c556a8c12d2
Matt Jankowski <mjankowski@thoughtbot.com>
2018-10-03 11:46:37 -0400

    Update rails to version 5.2.1

--- config/application.rb
+++ config/application.rb
@@ -1,13 +1,8 @@
 require_relative 'boot'
-require "rails"
-require "active_model/railtie"
-require "active_job/railtie"
-require "active_record/railtie"
-require "action_controller/railtie"
-require "action_view/railtie"
-require "action_cable/engine"
-require "sprockets/railtie"
+require 'rails/all'
+
 Bundler.require(*Rails.groups)
+
 module Bostonrb
   class Application < Rails::Application
     config.assets.quiet = true
