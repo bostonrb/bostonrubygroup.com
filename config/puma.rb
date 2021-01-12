717ba6d860c414ecc6b0ba2829a67726075c92bb
Peter Cai <222655+pcai@users.noreply.github.com>
2021-01-11 21:16:19 -0500

    remove activerecord (#57)

--- config/puma.rb
+++ config/puma.rb
@@ -36,5 +36,5 @@ plugin :tmp_restart
 on_worker_boot do
   # Worker specific setup for Rails 4.1+
   # See: https://devcenter.heroku.com/articles/deploying-rails-applications-with-the-puma-web-server#on-worker-boot
-  ActiveRecord::Base.establish_connection
+  # ActiveRecord::Base.establish_connection
 end
