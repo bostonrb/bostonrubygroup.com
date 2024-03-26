df01955dae5543928e26ff64a6e556e19ea014d2
Peter Cai <222655+pcai@users.noreply.github.com>
2024-03-26 17:31:39 +0000

    fix meetup api fetching

--- docs/application.js
+++ docs/application.js
@@ -4,17 +4,25 @@
   var request = new XMLHttpRequest();
 
   request.addEventListener("load", function () {
-    var event = JSON.parse(request.responseText);
+    const event_list = JSON.parse(request.responseText)["events"];
+    if (!event_list) {
+      message.innerHTML = `<p><a href="https://www.meetup.com/bostonrb">See upcoming events</a></p>`;
+      return;
+    }
+    const future_events = event_list.filter(e => e.status === "upcoming")
+      .sort((a, b) => a.start_time_raw - b.start_time_raw);
+    const event = future_events[0];
 
     if (event) {
       container.innerHTML = `
         <h2>Our next event</h2>
         <p class="events__item-title" data-role="event-name">${event.name}</p>
-        <p data-role="event-date-info"><time>${event.date}</time></p>
-        <p><time>${event.startTime}</time> to <time>${event.endTime}</time></p>
-        <address data-role="event-venue-info">${event.venueName}<br>${event.address}<br>${event.city}</address>
-        <p><a href="${event.rsvpURL}" class="cta--primary">RSVP</a></p>
-        <p><a href="${event.eventsURL}">See all events</a></p>
+        <p data-role="event-date-info"><time>${new Date(event.start_time_raw).toLocaleDateString(undefined, { year: 'numeric', month: 'long', day: 'numeric' })}</time></p>
+        <p><time>${event.start_time}</time> to <time>${event.end_time}</time></p>
+        <address data-role="event-venue-info">${event.location}<br></address>
+        <p><a href="${event.html_link}" class="cta--primary">RSVP</a></p>
+        <p>${event.description}</p>
+        <p><a href="https://www.meetup.com/bostonrb">See all events</a></p>
       `;
     } else {
       message.innerHTML = "No Upcoming Event";
@@ -22,14 +30,15 @@
   });
 
   request.addEventListener("error", function () {
-    message.innerHTML = "Error Fetching Events";
+    message.innerHTML = `<p><a href="https://www.meetup.com/bostonrb">See upcoming events</a></p>`;
   });
 
   request.addEventListener("timeout", function () {
-    message.innerHTML = "Timeout Fetching Events";
+    message.innerHTML = `<p><a href="https://www.meetup.com/bostonrb">See upcoming events</a></p>`;
   });
 
   request.timeout = 3000;
-  request.open("GET", "https://bostonrb.onrender.com/");
+  // https://www.sociablekit.com/app/users/widgets/update_embed/25388247
+  request.open("GET", `https://data.accentapi.com/feed/25388247.json?no_cache=${new Date().getTime()}`);
   request.send();
 })();
